3fe0a53e24cadc8db4ff8e7577529c76e25974bb
lihaoji <1224117846@qq.com>
2020-01-06 21:55:48 +0800

    jps -l : wins java 进程 jinfo -flag UserSerialGC pid : 是否开启XXX

---
+++ zhouyang/Juc/src/com/lhj/jvmgc/HelloGC.java
@@ -0,0 +1,14 @@
+package com.lhj.jvmgc;
+
+/**
+ * @author lihaoji
+ * @create 2020-01-05 20:57
+ */
+public class HelloGC {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("===================hello GC");
+
+        Thread.sleep(Integer.MAX_VALUE);
+        //jinfo -flag UseSerialGC 10004
+    }
+}
